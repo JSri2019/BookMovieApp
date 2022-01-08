4d9efced714939270d12106e6e717df80d52782c
Java Srivastava <javasri09@gmail.com>
2022-01-09 00:53:59 +0100

    Adding form details to Login tab

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -6,6 +6,12 @@ import Button from "@material-ui/core/Button";
 import Modal from "react-modal";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import PropTypes from "prop-types";
+import FormHelperText from "@material-ui/core/FormHelperText";
 
 const customStyles = {
   content: {
@@ -18,6 +24,18 @@ const customStyles = {
   },
 };
 
+const TabContainer = function (props) {
+  return (
+    <Typography component="div" style={{ padding: 0, textAlign: "center" }}>
+      {props.children}
+    </Typography>
+  );
+};
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
 class Header extends Component {
   constructor() {
     super();
@@ -25,11 +43,23 @@ class Header extends Component {
       isUserLoggedIn: false,
       isModalOpen: false,
       value: 0,
+      usernameRequired: "displayNone",
+      username: "",
+      loginPasswordRequired: "displayNone",
+      loginPassword: "",
     };
   }
 
+  // event handlers
   openModalHandler = () => {
-    this.setState({ isModalOpen: true });
+    this.setState({
+      isModalOpen: true,
+      value: 0,
+      usernameRequired: "displayNone",
+      username: "",
+      loginPasswordRequired: "displayNone",
+      loginPassword: "",
+    });
   };
 
   closeModalHandler = () => {
@@ -38,6 +68,23 @@ class Header extends Component {
 
   tabChangeHandler = (event, value) => {
     this.setState({ value });
+  };
+
+  loginClickHandler = () => {
+    this.state.username === ""
+      ? this.setState({ usernameRequired: "displayBlock" })
+      : this.setState({ usernameRequired: "displayNone" });
+    this.state.loginPassword === ""
+      ? this.setState({ loginPasswordRequired: "displayBlock" })
+      : this.setState({ loginPasswordRequired: "displayNone" });
+  };
+
+  usernameChangeHandler = (e) => {
+    this.setState({ username: e.target.value });
+  };
+
+  loginPasswordChangeHandler = (e) => {
+    this.setState({ loginPassword: e.target.value });
   };
 
   render() {
@@ -82,10 +129,55 @@ class Header extends Component {
           onRequestClose={this.closeModalHandler}
           style={customStyles}
         >
-          <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
+          <Tabs
+            className="tabs"
+            value={this.state.value}
+            onChange={this.tabChangeHandler}
+          >
             <Tab label="Login" />
             <Tab label="Register" />
           </Tabs>
+
+          {/* when login tab is selected */}
+          {this.state.value === 0 && (
+            <TabContainer>
+              <FormControl required>
+                <InputLabel htmlFor="username">Username</InputLabel>
+                <Input
+                  id="username"
+                  type="text"
+                  username={this.state.username}
+                  onChange={this.usernameChangeHandler}
+                />
+                <FormHelperText className={this.state.usernameRequired}>
+                  <span className="red">required</span>
+                </FormHelperText>
+              </FormControl>
+              <br />
+              <br />
+              <FormControl required>
+                <InputLabel htmlFor="loginPassword">Password</InputLabel>
+                <Input
+                  id="loginPassword"
+                  type="password"
+                  loginpassword={this.state.loginPassword}
+                  onChange={this.loginPasswordChangeHandler}
+                />
+                <FormHelperText className={this.state.loginPasswordRequired}>
+                  <span className="red">required</span>
+                </FormHelperText>
+              </FormControl>
+              <br />
+              <br />
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={this.loginClickHandler}
+              >
+                LOGIN
+              </Button>
+            </TabContainer>
+          )}
         </Modal>
       </div>
     );
