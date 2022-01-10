2b85e817b8c66bd36a89a18a13bf17b67c32feaf
Java Srivastava <javasri09@gmail.com>
2022-01-11 00:24:48 +0100

    Adding Left part and few sections in Middle part to Details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -2,18 +2,21 @@
 import React, { Component } from "react";
 import Header from "../../common/header/Header";
 import "./Details.css";
+import Typography from "@material-ui/core/Typography";
 
 class Details extends Component {
   constructor() {
     super();
     // initial state
     this.state = {
-      movie: {},
+      movie: { genres: [] },
     };
   }
 
+  /* Note: using componentDidMount instead of componentWillMount 
+  as the latter is deprecated and is giving warnings on console when used */
   componentDidMount() {
-    // getting movie information based on the movie id
+    // getting movie information based on the movie id using backend API
     let that = this;
     let bodyMovie = null;
     let xhrMovie = new XMLHttpRequest();
@@ -34,12 +37,54 @@ class Details extends Component {
   }
 
   render() {
+    let movie = this.state.movie;
     return (
       <div className="details">
         <Header />
         <div className="flex-container-details">
-          <div className="left-details"></div>
-          <div className="center-details"></div>
+          {/* left section */}
+          <div className="left-details">
+            <img src={movie.poster_url} alt={movie.title} />
+          </div>
+
+          {/* center section */}
+          <div className="center-details">
+            <div>
+              <Typography variant="headline" component="h2">
+                {movie.title}
+              </Typography>
+            </div>
+            <div>
+              <Typography>
+                {/* Note: keeping the label as 'Genre' and not 'Genres' by following the screenshot */}
+                <span className="bold">Genre:</span> {movie.genres.join(", ")}
+              </Typography>
+            </div>
+            <div>
+              <Typography>
+                <span className="bold">Duration:</span> {movie.duration}
+              </Typography>
+            </div>
+            <div>
+              <Typography>
+                <span className="bold">Release Date:</span>{" "}
+                {new Date(movie.release_date).toDateString()}
+              </Typography>
+            </div>
+            <div>
+              <Typography>
+                <span className="bold">Rating:</span> {movie.rating}
+              </Typography>
+            </div>
+            <div className="top-margin">
+              <Typography>
+                <span className="bold">Plot:</span>{" "}
+                <a href={movie.wiki_url}>(Wiki Link)</a> {movie.storyline}
+              </Typography>
+            </div>
+          </div>
+
+          {/* right section */}
           <div className="right-details"></div>
         </div>
       </div>
